0c5932f136c1d57c14804c3fbee02155d9fac824
marcoamabili <marcoamabili@gmail.com>
2018-01-10 17:58:48 +0100

    PB_LESSON49

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rb;
+	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();      
+    }
+	
+
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(- Vector3.forward);
+        }
+    }
+}
